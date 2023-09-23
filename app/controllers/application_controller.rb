ce3178e25ca949a849c75a9f7ed8df0013f35699
Dibyajit Goswami <dibyajitgoswami@Dibyajits-MacBook-Pro.local>
2023-09-23 23:57:58 +0530

    Api Request Logs added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  after_action :log_request_response
+
+  private
+
+  def log_request_response
+    @current_request = ApiRequest.create!(
+      endpoint: request.fullpath,
+      payload: request.params.to_json,
+      response: response.body + ' status: ' + response.code
+    )
+  end
 end
